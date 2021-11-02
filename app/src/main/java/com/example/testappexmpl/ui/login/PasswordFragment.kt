4915b4fe78a46e89d6f72ddb2b4546bcd8b872cb
f.r.malikov <fuad.malikov@rabitabank.com>
2021-11-02 15:04:47 +0400

    Task app.

---
+++ app/src/main/java/com/example/testappexmpl/ui/login/PasswordFragment.kt
@@ -0,0 +1,22 @@
+package com.example.testappexmpl.ui.login
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.testappexmpl.R
+import com.example.testappexmpl.databinding.FragmentPasswordBinding
+
+class PasswordFragment : Fragment() {
+    private val binding by lazy { FragmentPasswordBinding.inflate(layoutInflater) }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+
+        return binding.root
+    }
+}
