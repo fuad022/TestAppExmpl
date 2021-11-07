a46a296578aab50110aca17baf14de5a25cd87c9
f.r.malikov <fuad.malikov@rabitabank.com>
2021-11-07 12:58:39 +0400

    Some changes.

---
+++ app/src/main/java/com/example/testappexmpl/ui/postpagedetails/PostPageDetailsFragment.kt
@@ -0,0 +1,21 @@
+package com.example.testappexmpl.ui.postpagedetails
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.testappexmpl.R
+import com.example.testappexmpl.databinding.FragmentPostPageDetailsBinding
+
+class PostPageDetailsFragment : Fragment() {
+    private val binding by lazy { FragmentPostPageDetailsBinding.inflate(layoutInflater) }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        return binding.root
+    }
+}
