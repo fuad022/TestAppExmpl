f958f82bcd6ff2c091cc7d5330a2b0a71fa1a7fd
f.r.malikov <fuad.malikov@rabitabank.com>
2021-11-08 17:45:18 +0400

    Some changes.

--- app/src/main/java/com/example/testappexmpl/data/mocklist/MockData.kt
+++ app/src/main/java/com/example/testappexmpl/data/mocklist/MockData.kt
@@ -17,26 +17,26 @@ object MockData {
 
     fun getHomeVerticalList(): List<HomeVerticalRecyclerItemModel> {
         val list: ArrayList<HomeVerticalRecyclerItemModel> = arrayListOf()
-        list.add(HomeVerticalRecyclerItemModel("Child", getHomeGridList1(), "Quickly stabilize your baby’s emotions"))
-        list.add(HomeVerticalRecyclerItemModel("Nature", getHomeGridList2(), "Quickly stabilize your baby’s emotions"))
+        list.add(HomeVerticalRecyclerItemModel("Fitness and run clubs", getHomeGridList1(), "See all Fitness and run clubs"))
+        list.add(HomeVerticalRecyclerItemModel("Healthy eating", getHomeGridList2(), "See all healthy eating "))
         return list
     }
 
     private fun getHomeGridList1(): List<HomeNestedGridRecyclerItemModel> {
         val list: ArrayList<HomeNestedGridRecyclerItemModel> = arrayListOf()
-        list.add(HomeNestedGridRecyclerItemModel(1, "Female voice",""))
-        list.add(HomeNestedGridRecyclerItemModel(2, "Female voice",""))
-        list.add(HomeNestedGridRecyclerItemModel(3, "Female voice",""))
-        list.add(HomeNestedGridRecyclerItemModel(4, "Female voice",""))
+        list.add(HomeNestedGridRecyclerItemModel(R.drawable.vertical_img_1, "Beach run club","Run along with waves"))
+        list.add(HomeNestedGridRecyclerItemModel(R.drawable.vertical_img_2, "Boarding","Skate around your local city"))
+        list.add(HomeNestedGridRecyclerItemModel(R.drawable.vertical_img_3, "Climb","End of week climb headspace"))
+        list.add(HomeNestedGridRecyclerItemModel(R.drawable.vertical_img_4, "Body","Train and breathe with Manal"))
         return list
     }
 
     private fun getHomeGridList2(): List<HomeNestedGridRecyclerItemModel> {
         val list: ArrayList<HomeNestedGridRecyclerItemModel> = arrayListOf()
-        list.add(HomeNestedGridRecyclerItemModel(5, "Female voice",""))
-        list.add(HomeNestedGridRecyclerItemModel(6, "Female voice",""))
-        list.add(HomeNestedGridRecyclerItemModel(7, "Female voice",""))
-        list.add(HomeNestedGridRecyclerItemModel(8, "Female voice",""))
+        list.add(HomeNestedGridRecyclerItemModel(R.drawable.vertical_img_5, "Vegan café","Essential Vegan"))
+        list.add(HomeNestedGridRecyclerItemModel(R.drawable.vertical_img_6, "Food and coffee","Kahalia Roden café"))
+        list.add(HomeNestedGridRecyclerItemModel(R.drawable.vertical_img_7, "Asian cuisine","On the Bab"))
+        list.add(HomeNestedGridRecyclerItemModel(R.drawable.vertical_img_8, "Lunch and dinner","Fresh café"))
         return list
     }
 
