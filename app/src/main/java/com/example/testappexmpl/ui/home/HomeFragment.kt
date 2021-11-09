845e9b0bfe0d83bc5e87e097522a6b362e2c4723
f.r.malikov <fuad.malikov@rabitabank.com>
2021-11-09 22:41:02 +0400

    Some changes.

--- app/src/main/java/com/example/testappexmpl/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/testappexmpl/ui/home/HomeFragment.kt
@@ -1,6 +1,7 @@
 package com.example.testappexmpl.ui.home
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -49,6 +50,7 @@ class HomeFragment : Fragment() {
 
     private fun setupGridAdapter() {
         homeNestedGridRecyclerAdapter.setOnClickListener { PostPageModel ->
+            Log.d("HomeFragment", "setupGridAdapter_setOnClickListener")
             PostPageModel.let {
                 findNavController().navigate(
                     HomeFragmentDirections.actionHomeFragmentToPostPageDetailsFragment(PostPageModel)
