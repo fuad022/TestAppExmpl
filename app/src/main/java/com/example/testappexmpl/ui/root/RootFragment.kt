0ede19db86a0294b186eb1101cb228622c81a193
f.r.malikov <fuad.malikov@rabitabank.com>
2021-11-22 15:39:24 +0400

    Some changes.

--- app/src/main/java/com/example/testappexmpl/ui/root/RootFragment.kt
+++ app/src/main/java/com/example/testappexmpl/ui/root/RootFragment.kt
@@ -44,14 +44,14 @@ class RootFragment : Fragment() {
             }
         }
 
-        val userProfileUrl =
-            "https://i.pinimg.com/originals/fd/1f/79/fd1f79bccedb91b28bebeaf2f84159f3.jpg"
-
-        binding.bottomNav.selectedItemId = R.id.home
-
-        binding.bottomNav.loadImage(
-            userProfileUrl, R.id.profile, R.drawable.ic_launcher_foreground
-        )
+//        val userProfileUrl =
+//            "https://i.pinimg.com/originals/fd/1f/79/fd1f79bccedb91b28bebeaf2f84159f3.jpg"
+//
+//        binding.bottomNav.selectedItemId = R.id.home
+//
+//        binding.bottomNav.loadImage(
+//            userProfileUrl, R.id.profile, R.drawable.ic_launcher_foreground
+//        )
     }
 
     private fun hideBottomNav() {
