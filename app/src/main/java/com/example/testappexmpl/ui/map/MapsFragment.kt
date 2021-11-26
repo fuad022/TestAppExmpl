179c2d899f764cc936112485da31dda391686649
f.r.malikov <fuad.malikov@rabitabank.com>
2021-11-26 10:00:38 +0400

    Some changes.

--- app/src/main/java/com/example/testappexmpl/ui/map/MapsFragment.kt
+++ app/src/main/java/com/example/testappexmpl/ui/map/MapsFragment.kt
@@ -85,7 +85,7 @@ class MapsFragment : Fragment(), OnMapReadyCallback, GoogleMap.OnMarkerClickList
                         .icon(
                             bitmapDescriptorFromVector(
                                 requireActivity(),
-                                R.drawable.ic_map_pin_new
+                                R.drawable.ic_map_pin_circle
                             )
                         )
                 )
